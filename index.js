80233cbfe5f3c83332b6cd0596187179d0b64245
Bayarbold <arrowbold@gmail.com>
2021-08-04 14:49:36 +0800

    Test bolgoson n

---
+++ index.js
@@ -0,0 +1,18 @@
+warning: LF will be replaced by CRLF in src/index.js.
+The file will have its original line endings in your working directory
+[1mdiff --git a/src/index.js b/src/index.js[m
+[1mindex 70e7885..9ba8d29 100644[m
+[1m--- a/src/index.js[m
+[1m+++ b/src/index.js[m
+[36m@@ -4,8 +4,9 @@[m [mimport "./index.css";[m
+ import App from "./App";[m
+ import reportWebVitals from "./reportWebVitals";[m
+ import "bootstrap/dist/css/bootstrap.css";[m
+[31m-import Tests from "./components/tests";[m
+[32m+[m[32mimport Test from "./components/test";[m
+ [m
+[31m-ReactDOM.render(<Tests />, document.getElementById("root"));[m
+[32m+[m[32mReactDOM.render(<Test />, document.getElementById("root"));[m
+[32m+[m[32m//ssaihan bnaaaa[m
+ [m
+ reportWebVitals();[m
